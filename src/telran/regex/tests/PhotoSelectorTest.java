75a91c885eaf14f5f1d2ad296afbfd68d2bbcfcc
TimurMP <titimurmamyshev@gmail.commur>
2022-06-14 21:03:04 +0300

    added testAllAutumnPictures + reworked selectPictures method

--- src/telran/regex/tests/PhotoSelectorTest.java
+++ src/telran/regex/tests/PhotoSelectorTest.java
@@ -49,4 +49,20 @@ class PhotoSelectorTest {
         assertArrayEquals(expected, actual);
 
     }
+    @Test
+    void testAllAutumnPictures() {
+        String regex = "\\d{4}(09|10|11)\\d{2}";
+        String[] actual = PhotoSelector.selectPictures(pictures, regex);
+        String[] expected = {
+                "London\\20151001_110023.jpg",
+                "London\\20151001_121705.jpg",
+                "London\\20151001_231035.jpg",
+                "Chicago\\20151111_232000.png"
+        };
+        assertArrayEquals(expected, actual);
+
+    }
+
+
+
 }
--- src/telran/regex/tools/PhotoSelector.java
+++ src/telran/regex/tools/PhotoSelector.java
@@ -5,26 +5,18 @@ import java.util.regex.Pattern;
 
 public class PhotoSelector {
     public static String[] selectPictures(String[] pictures, String regex) {
+//        "London\\20151001_110023.jpg",
+//        regex = "\\d{4}[09|10|11]\\d{2}";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher;
-        int matcherCounter = 0;
-
+        StringBuilder builder = new StringBuilder();
         for (int i = 0; i < pictures.length; i++) {
             matcher = pattern.matcher(pictures[i]);
             if (matcher.find()) {
-                matcherCounter++;
+                builder.append(pictures[i]+",");
             }
         }
-        String[] arr = new String[matcherCounter];
-
-        for (int i = 0; i < pictures.length; i++) {
-            matcher = pattern.matcher(pictures[i]);
-            if (matcher.find()) {
-                arr[i] = pictures[i];
-            }
-
-
-        }
+        String[] arr = builder.toString().split(",");
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
 
