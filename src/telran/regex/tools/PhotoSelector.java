a2c97b94f47ee7856aa247cbe425555a377c3d06
TimurMP <titimurmamyshev@gmail.commur>
2022-06-15 00:17:08 +0300

    Final 1.5

--- src/telran/regex/tools/PhotoSelector.java
+++ src/telran/regex/tools/PhotoSelector.java
@@ -5,8 +5,6 @@ import java.util.regex.Pattern;
 
 public class PhotoSelector {
     public static String[] selectPictures(String[] pictures, String regex) {
-//        Chicago\20151111_232000.png,
-//        regex = "1[8-9]|2[1-4]\\d{4}";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher;
         StringBuilder builder = new StringBuilder();
